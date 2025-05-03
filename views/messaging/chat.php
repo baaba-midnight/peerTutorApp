0d051b197a22f830e39e2ff328d849d48983f2d6
senseicudjoe <135157084+senseicudjoe@users.noreply.github.com>
2025-05-03 10:05:37 -1200

    Fixed bugs
    Fixed the issue with the User model

--- views/messaging/chat.php
+++ views/messaging/chat.php
@@ -209,7 +209,7 @@
         </div>
     </div>
 
-    <?php include('../../includes/footer.php'); ?>
+
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
